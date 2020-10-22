4ab5ee98d57ebe9716a8a29a4f9b92e7e3c37ecc
Paul <molinpaul@live.fr>
2020-10-22 16:58:50 +0200

    Refac

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 const config = {
     database : {
-        uri: "mongodb+srv://administrateur:ywa4wOF4PWGVxM40@cluster0-ds6hm.gcp.mongodb.net/auth?retryWrites=true&w=majority"
+        uri: process.env.MONGODB_URI
     },
     token: {
-        secret: "gFu6_lpM30)ko2&ijUyHGvR5g784",
+        secret: process.env.SECRET,
         access: {
             expiry: 60 * 60
         },
